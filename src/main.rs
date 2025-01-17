124b9ec5a8779d2519f93b97bf5569ca9df9cb60
arch_il <archilberaia+github@gmail.com>
2025-01-17 22:34:23 +0400

    add window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::{color, window};
+
+fn window_conf() -> window::Conf {
+    window::Conf {
+        window_title: "Card Engine".to_owned(),
+        window_resizable: false,
+        window_width: 806,
+        window_height: 500,
+        high_dpi: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        window::clear_background(color::BLACK);
+
+        window::next_frame().await
+    }
 }
